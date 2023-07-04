ff7db6536133e0d09a51146872cf7c7421af0df9
CakraDarma <baguscakraa02@gmail.com>
2023-07-04 19:49:15 +0800

    add nav config

---
+++ src/config/nav.ts
@@ -0,0 +1,20 @@
+export const navConfig = {
+	mainNav: [
+		{
+			title: 'Kabupaten',
+			href: '/kabupaten',
+		},
+		{
+			title: 'Kecamatan',
+			href: '/kecamatan',
+		},
+		{
+			title: 'Desa',
+			href: '/desa',
+		},
+		{
+			title: 'Pura',
+			href: '/pura',
+		},
+	],
+};
