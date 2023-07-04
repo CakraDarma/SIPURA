ca7cb2b3a7d713733f17563d52ef989dba8e181b
CakraDarma <baguscakraa02@gmail.com>
2023-07-05 01:00:33 +0800

    add custom color tailwind

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,21 +6,37 @@ import { buttonVariants } from './ui/Button';
 import { UserAccountNav } from './UserAccountNav';
 import SearchBar from './SearchBar';
 
+import { navConfig } from '@/config/nav';
+
 const Navbar = async () => {
 	const session = await getServerSession(authOptions);
 	return (
-		<div className='fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2'>
+		<div className='fixed top-0 inset-x-0 h-fit bg-black-light border-b border-zinc-300 z-[10] py-2'>
 			<div className='container max-w-7xl h-full mx-auto flex items-center justify-between gap-2'>
 				{/* logo */}
 				<Link href='/' className='flex gap-2 items-center'>
 					<Icons.logo className='h-8 w-8 sm:h-6 sm:w-6' />
-					<p className='hidden text-zinc-700 text-sm font-medium md:block'>
+					<p className='hidden text-primary text-sm font-medium md:block'>
 						Breadit
 					</p>
 				</Link>
 
-				{/* search bar */}
-				<SearchBar />
+				{/* navigation */}
+				{navConfig?.length ? (
+					<nav className='hidden gap-6 md:flex'>
+						{navConfig?.map((item, index) => (
+							<Link
+								key={index}
+								href={item.href}
+								className={
+									'flex items-center text-lg font-medium transition-colors hover:text-foreground/80 md:text-sm text-foreground'
+								}
+							>
+								{item.title}
+							</Link>
+						))}
+					</nav>
+				) : null}
 
 				{/* actions */}
 				{session?.user ? (
@@ -30,6 +46,9 @@ const Navbar = async () => {
 						Sign In
 					</Link>
 				)}
+
+				{/* search bar */}
+				<SearchBar />
 			</div>
 		</div>
 	);
--- src/config/nav.ts
+++ src/config/nav.ts
@@ -1,20 +1,18 @@
-export const navConfig = {
-	mainNav: [
-		{
-			title: 'Kabupaten',
-			href: '/kabupaten',
-		},
-		{
-			title: 'Kecamatan',
-			href: '/kecamatan',
-		},
-		{
-			title: 'Desa',
-			href: '/desa',
-		},
-		{
-			title: 'Pura',
-			href: '/pura',
-		},
-	],
-};
+export const navConfig = [
+	{
+		title: 'Kabupaten',
+		href: '/kabupaten',
+	},
+	{
+		title: 'Kecamatan',
+		href: '/kecamatan',
+	},
+	{
+		title: 'Desa',
+		href: '/desa',
+	},
+	{
+		title: 'Pura',
+		href: '/pura',
+	},
+];
