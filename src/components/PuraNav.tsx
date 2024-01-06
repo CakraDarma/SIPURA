07c165f2555e9b17afd168cc1aac9a50d73d6549
CakraDarma <baguscakraa02@gmail.com>
2024-01-06 14:21:07 +0800

    fix: borderline pura navigation

--- src/components/PuraNav.tsx
+++ src/components/PuraNav.tsx
@@ -9,8 +9,10 @@ export default function PuraNav() {
 	const params = useParams();
 	const path = usePathname();
 
+	console.log(path);
+
 	return (
-		<div className='bg-gray-200 '>
+		<div className='bg-gray-200'>
 			<div className='container flex flex-row justify-center w-full gap-12 py-3 max-w-7xl'>
 				{puraNavConfig?.map((item, index) => (
 					<Link
@@ -18,7 +20,12 @@ export default function PuraNav() {
 						href={`/pura/${params.puraId}/${item.href}`}
 						className={`font-sans text-sm font-medium  
             ${
-							path.includes(item.href) ? 'border-b-2 border-orange-light' : ''
+							(path.includes(item.href) && item.href !== '/') ||
+							(item.href === '/' &&
+								(path === `/pura/${params.puraId}` ||
+									path === `/pura/${params.puraId}/`))
+								? 'border-b-2 border-orange-light'
+								: ''
 						}`}
 					>
 						{item.title}
