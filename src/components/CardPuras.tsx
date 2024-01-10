78608481ffc6f45f353c07da2c9a6cc10779371f
CakraDarma <baguscakraa02@gmail.com>
2024-01-11 01:05:18 +0800

    fix: pura operations in cardpuras

--- src/components/CardPuras.tsx
+++ src/components/CardPuras.tsx
@@ -37,37 +37,39 @@ export default async function CardPuras({ pura, link }: CardPuraProps) {
 
 	const session = await getAuthSession();
 	return (
-		<Link href={link}>
-			<div className='relative flex w-full max-w-[23rem] flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-lg'>
-				<div className='relative mx-4 mt-4 overflow-hidden text-white shadow-lg rounded-xl bg-blue-gray-500 bg-clip-border shadow-blue-gray-500/40'>
-					<Image
-						src={`${pura.thumbnail}`}
-						alt='Announce'
-						width={1000}
-						height={1000}
-						quality={100}
-						className='w-[336px] h-[224px] object-cover'
-					/>
-				</div>
-				<div className='p-6'>
-					<div className='flex items-center justify-between mb-3'>
-						<h5 className='block font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900'>
-							{pura.name}
-						</h5>
+		<div className='relative flex w-full max-w-[23rem] flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-lg'>
+			<Link href={link}>
+				<div>
+					<div className='relative mx-4 mt-4 overflow-hidden text-white shadow-lg rounded-xl bg-blue-gray-500 bg-clip-border shadow-blue-gray-500/40'>
+						<Image
+							src={`${pura.thumbnail}`}
+							alt='Announce'
+							width={1000}
+							height={1000}
+							quality={100}
+							className='w-[336px] h-[224px] object-cover'
+						/>
 					</div>
-					<p className='block font-sans text-sm antialiased font-light leading-relaxed text-gray-500'>
-						{formattedResult}
-					</p>
+					<div className='p-6'>
+						<div className='flex items-center justify-between mb-3'>
+							<h5 className='block font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900'>
+								{pura.name}
+							</h5>
+						</div>
+						<p className='block font-sans text-sm antialiased font-light leading-relaxed text-gray-500'>
+							{formattedResult}
+						</p>
+					</div>
 				</div>
-				<div className='flex flex-row justify-end w-full'>
-					{
-						// @ts-ignore
-						session?.user.role === 'ADMIN' ? (
-							<PuraOperations pura={{ id: pura.id, name: pura.name }} />
-						) : null
-					}
-				</div>
+			</Link>
+			<div className='flex flex-row justify-end w-full'>
+				{
+					// @ts-ignore
+					session?.user.role === 'ADMIN' ? (
+						<PuraOperations pura={{ id: pura.id, name: pura.name }} />
+					) : null
+				}
 			</div>
-		</Link>
+		</div>
 	);
 }
