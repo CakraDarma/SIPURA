aa2586b2a7e0e3f2aaa3d9a69c7345f881ff1004
CakraDarma <baguscakraa02@gmail.com>
2024-03-22 14:15:44 +0800

    style: editor output editor js

--- src/components/EditorOutput.tsx
+++ src/components/EditorOutput.tsx
@@ -4,6 +4,71 @@ import CustomCodeRenderer from '@/components/renderers/CustomCodeRenderer';
 import CustomImageRenderer from '@/components/renderers/CustomImageRenderer';
 import dynamic from 'next/dynamic';
 
+const blogContentStyle = {
+	header: {
+		h1: {
+			fontSize: '2.5rem',
+			fontWeight: 'bold',
+			margin: '28px 0 10px 0',
+		},
+		h2: {
+			fontSize: '2rem',
+			fontWeight: 'bold',
+			margin: '28px 0 10px 0',
+		},
+		h3: {
+			fontSize: '1.75rem',
+			fontWeight: 'bold',
+			margin: '28px 0 10px 0',
+		},
+		h4: {
+			fontSize: '1.5rem',
+			fontWeight: 'bold',
+			margin: '28px 0 10px 0',
+		},
+		h5: {
+			fontSize: '1.25rem',
+			fontWeight: 'bold',
+			margin: '28px 0 10px 0',
+		},
+		h6: {
+			fontSize: '1rem',
+			fontWeight: 'bold',
+			margin: '28px 0 10px 0',
+		},
+	},
+	list: {
+		container: {
+			marginLeft: '2rem',
+		},
+		listItem: {
+			padding: '.1rem 2rem',
+			listStyleType: 'disc',
+			fontSize: '1.3rem',
+			lineHeight: '2.4rem',
+			marginBottom: '10px',
+		},
+	},
+	paragraph: {
+		fontSize: '1.3rem',
+		textAlign: 'justify',
+		lineHeight: '2.4rem',
+		marginBottom: '18px',
+	},
+};
+
+const blogContentConfig = {
+	header: {
+		disableDefaultStyle: true,
+	},
+	paragraph: {
+		disableDefaultStyle: true,
+	},
+	list: {
+		disableDefaultStyle: true,
+	},
+};
+
 const Output = dynamic(
 	async () => (await import('editorjs-react-renderer')).default,
 	{ ssr: false }
@@ -18,18 +83,15 @@ const renderers = {
 	code: CustomCodeRenderer,
 };
 
-const style = {
-	paragraph: {
-		fontSize: '1.3rem',
-		// fontWeight: 200,
-		textAlign: 'justify',
-		lineHeight: '2.4rem',
-		marginBottom: '18px',
-	},
-};
-
 const EditorOutput = ({ content }: EditorOutputProps) => {
-	return <Output style={style} renderers={renderers} data={content} />;
+	return (
+		<Output
+			style={blogContentStyle}
+			config={blogContentConfig}
+			renderers={renderers}
+			data={content}
+		/>
+	);
 };
 
 export default EditorOutput;
